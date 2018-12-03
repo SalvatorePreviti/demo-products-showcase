f7582fc65abe2497e39829192774288422e4b42a
Salvatore Previti <roorback@gmail.com>
2018-12-03 15:46:35 +0000

    add whatwg-fetch

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,7 @@
+// Polyfill for Internet Explorer and other browsers that don't support 'fetch'
+// See https://github.github.io/fetch/
+import 'whatwg-fetch'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App/App'
